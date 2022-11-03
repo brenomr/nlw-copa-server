d23a5deb3111c521ca422575b7d4607b339ca2e8
Breno Rodrigues <brenocartoon@hotmail.com>
2022-11-02 21:01:47 -0300

    feat: creating new routes  to create pools and count users and guesses

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import ShortUniqueId from "short-unique-id"; 'short-unique-id';
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -19,6 +21,34 @@ async function bootstrap() {
         const count = await prisma.pool.count();
 
         return { count };
+    });
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count };
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count };
+    });
+
+    fastify.post('/pools', async (req, res) => {
+        const createPoolBody = z.object({
+            title: z.string().min(1)
+        });
+        const { title } = createPoolBody.parse(req.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        const result = await prisma.pool.create({
+            data: { title, code, }
+        });
+
+        return res.status(201).send({ result });
     });
 
     fastify.listen({ port: 3000 });
